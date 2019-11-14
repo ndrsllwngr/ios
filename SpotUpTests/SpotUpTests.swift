31874b077b3e02f02c1bc5970df88af928e5d120
Timo Erdelt <ti.erdelt@gmail.com>
2019-11-14 19:41:30 +0100

    Add empty xcode project

---
+++ SpotUpTests/SpotUpTests.swift
@@ -0,0 +1,34 @@
+//
+//  SpotUpTests.swift
+//  SpotUpTests
+//
+//  Created by Timo Erdelt on 14.11.19.
+//  Copyright © 2019 iOS WiSe 19/20 Gruppe 7. All rights reserved.
+//
+
+import XCTest
+@testable import SpotUp
+
+class SpotUpTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
