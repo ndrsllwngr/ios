5136ed44b9d2a07914409800ffe794d3271defe9
Fangli Lu <fangli.lu@outlook.de>
2019-12-05 18:11:15 +0100

    add modules GoogleMaps and GoogplePlaces

--- SpotUp/AppDelegate.swift
+++ SpotUp/AppDelegate.swift
@@ -8,16 +8,19 @@
 
 import UIKit
 import Firebase
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        GMSServices.provideAPIKey("AIzaSyBJgMwNKkk8i8Ue5TmmLHDrwoNyO5iYMMQ")
+        GMSPlacesClient.provideAPIKey("AIzaSyBJgMwNKkk8i8Ue5TmmLHDrwoNyO5iYMMQ")
         return true
     }
 
