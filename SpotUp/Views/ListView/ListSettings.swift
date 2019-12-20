a3eef53c41398b8fbfea24b9f453104f61b70353
Fangli Lu <fangli.lu@outlook.de>
2019-12-20 15:43:14 +0100

    edit placeList name is available

---
+++ SpotUp/Views/ListView/ListSettings.swift
@@ -0,0 +1,20 @@
+//
+//  ListSettings.swift
+//  SpotUp
+//
+//  Created by Fangli Lu on 20.12.19.
+//
+
+import SwiftUI
+
+struct ListSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListSettings_Previews: PreviewProvider {
+    static var previews: some View {
+        ListSettings()
+    }
+}
