83c8d0486c6db5aa01da09838c12cfe6ae8c5eb6
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2019-12-22 16:49:56 +0100

    Improve HomeView, add links to Profile and ListView

--- SpotUp/Views/HomeView/ListsResults.swift
+++ SpotUp/Views/HomeView/ListsResults.swift
@@ -8,15 +8,14 @@
 import SwiftUI
 
 struct ListsResults: View {
-    @Binding var searchQuery: String
+
     var body: some View {
-        Text(searchQuery)
+        Text("Lists")
     }
 }
 
 struct ListsResults_Previews: PreviewProvider {
-    @State static var searchQuery = "timo"
     static var previews: some View {
-        ListsResults(searchQuery: $searchQuery)
+        ListsResults()
     }
 }
