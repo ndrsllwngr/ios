779856ce947a69b4547849232b8c067a9213fb05
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2019-12-23 00:11:32 +0100

    Init Google Places search

--- SpotUp/Views/HomeView/HomeView.swift
+++ SpotUp/Views/HomeView/HomeView.swift
@@ -7,14 +7,23 @@
 //
 
 import SwiftUI
+import GooglePlaces
 
 struct HomeView: View {
     @State private var selection = "places"
     @State private var searchTerm: String = ""
     @ObservedObject var searchSpace = FirestoreSearch()
     let searchController = UISearchController(searchResultsController: nil)
-    var filteredUsers: [User] = []
-    var filteredLists: [PlaceList] = []
+    @State private var googlePlaces: [GMSAutocompletePrediction] = []
+    
+    /**
+     * Create a new session token. Be sure to use the same token for calling
+     * findAutocompletePredictions, as well as the subsequent place details request.
+     * This ensures that the user's query and selection are billed as a single session.
+     */
+    let token = GMSAutocompleteSessionToken.init()
+    // Create a type filter.
+    let filter = GMSAutocompleteFilter()
     
     var body: some View {
         
@@ -40,6 +49,12 @@ struct HomeView: View {
                     
                     if selection == "places" {
                         VStack{
+                            Button(action: {self.GSM()}){Text("PUSH")}
+                            List{
+                                ForEach(self.googlePlaces, id: \.placeID) {
+                                    place in Text("\(place.placeID)")
+                                }
+                            }
                             Spacer()
                         }
                         
@@ -78,6 +93,41 @@ struct HomeView: View {
             self.searchSpace.cleanAllUsers()
         }
     }
+    
+    func GSM() {
+        self.filter.type = .establishment
+        
+        placesClient.findAutocompletePredictions(fromQuery: self.searchTerm,
+                                                 bounds: nil,
+                                                 boundsMode: GMSAutocompleteBoundsMode.bias,
+                                                 filter: filter,
+                                                 sessionToken: token,
+                                                 callback: { (results, error) in
+                                                    if let error = error {
+                                                        print("Autocomplete error: \(error)")
+                                                        return
+                                                    }
+                                                    if let results = results {
+                                                        for result in results {
+                                                            print("Result \(result.attributedFullText) with placeID \(result.placeID) and distance \(result.distanceMeters)")
+                                                        }
+                                                        self.googlePlaces = results
+                                                    }
+        })
+        
+        //        let regularFont = UIFont.systemFont(ofSize: UIFont.labelFontSize)
+        //        let boldFont = UIFont.boldSystemFont(ofSize: UIFont.labelFontSize)
+        
+        //        let bolded = prediction.attributedFullText.mutableCopy() as! NSMutableAttributedString
+        //        bolded.enumerateAttribute(kGMSAutocompleteMatchAttribute, in: NSMakeRange(0, bolded.length), options: []) {
+        //          (value, range: NSRange, stop: UnsafeMutablePointer<ObjCBool>) -> Void in
+        //            let font = (value == nil) ? regularFont : boldFont
+        //            bolded.addAttribute(NSFontAttributeName, value: font, range: range)
+        //        }
+        //
+        //        label.attributedText = bolded
+    }
+    
 }
 
 
