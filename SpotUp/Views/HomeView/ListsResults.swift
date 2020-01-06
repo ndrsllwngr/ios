0d1f1f177ef65d180fbd9530a79d6baf0ff7793b
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2020-01-06 23:32:32 +0100

    Update Google Search

--- SpotUp/Views/HomeView/ListsResults.swift
+++ SpotUp/Views/HomeView/ListsResults.swift
@@ -6,16 +6,25 @@
 //
 
 import SwiftUI
+import GooglePlaces
 
 struct ListsResults: View {
-
+    @Binding var googlePlaces: [GMSAutocompletePrediction]
     var body: some View {
-        Text("Lists")
+        List { ForEach(self.googlePlaces, id: \.placeID) {
+            result in HStack {
+//                Text(result.placeID)
+                Text(result.attributedFullText.string)
+                Spacer()
+                }
+            }
+            Spacer()
+        }
     }
 }
 
-struct ListsResults_Previews: PreviewProvider {
-    static var previews: some View {
-        ListsResults()
-    }
-}
+//struct ListsResults_Previews: PreviewProvider {
+//    static var previews: some View {
+////        ListsResults()
+//    }
+//}
