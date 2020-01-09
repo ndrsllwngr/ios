641f34c36b2a88ffc3bb91b3efe8f59612c60b5c
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2020-01-09 12:42:29 +0100

    Add empty state to search
    Co-authored-by: Timo Erdelt <ti.erdelt@gmail.com>

---
+++ SpotUp/Views/HomeView/SearchResultsPlaceLists.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResultsPlaceLists.swift
+//  SpotUp
+//
+//  Created by Andreas Ellwanger on 09.01.20.
+//
+
+import SwiftUI
+
+struct SearchResultsPlaceLists: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchResultsPlaceLists_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchResultsPlaceLists()
+    }
+}
