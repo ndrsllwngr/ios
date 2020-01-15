350be9a1073efda0f8eb79508186157d6a956689
Timo Erdelt <ti.erdelt@gmail.com>
2020-01-15 14:34:02 +0100

    Add placeListImage
    Gets uploaded from first place that gets added to the list

--- SpotUp/Views/FirebaseImage/FirebaseImages.swift
+++ SpotUp/Views/FirebaseImage/FirebaseImages.swift
@@ -41,3 +41,44 @@ struct FirebaseProfileImage: View {
         
     }
 }
+
+struct FirebasePlaceListImage: View {
+    
+    var imageUrl: String?
+    
+    var body: some View {
+        
+        VStack {
+            if (imageUrl != nil) {
+                WebImage(url: URL(string: imageUrl!))
+                    .onSuccess { image, cacheType in
+                        // Success
+                }
+                    .resizable() // Resizable like SwiftUI.Image
+                    .placeholder(Image(systemName: "placelist_image_placeholder")) // Placeholder Image
+                    // Supports ViewBuilder as well
+                    .placeholder {
+                        Rectangle().foregroundColor(.gray)
+                }
+                    .animated() // Supports Animated Image
+                    .indicator(.activity) // Activity Indicator
+                    .animation(.easeInOut(duration: 0.5)) // Animation Duration
+                    .transition(.fade) // Fade Transition
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .clipShape(Circle())
+            } else {
+                Image(uiImage: UIImage(named: "placelist_image_placeholder")!)
+                    .resizable()
+                    .animation(.easeInOut(duration: 0.5))
+                    .transition(.fade)
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .clipShape(Circle())
+            }
+            
+        }
+        
+    }
+}
+
