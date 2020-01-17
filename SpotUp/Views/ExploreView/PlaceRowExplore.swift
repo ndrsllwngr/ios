1720ebac0c274ed5c0caa518b6104f718d05f4b7
Timo Erdelt <ti.erdelt@gmail.com>
2020-01-17 16:29:00 +0100

    Navigate to current target with google maps

--- SpotUp/Views/ExploreView/PlaceRowExplore.swift
+++ SpotUp/Views/ExploreView/PlaceRowExplore.swift
@@ -66,3 +66,65 @@ struct PlaceRowExplore: View {
         }
     }
 }
+
+
+struct CurrentTargetRow: View {
+    var place: ExplorePlace
+    
+    @ObservedObject var exploreModel = ExploreModel.shared
+    
+    @State var image: UIImage? = nil
+    
+    var body: some View {
+        GeometryReader { metrics in
+            HStack(alignment: .center) {
+                HStack {
+                    PlaceRowImage(image: self.image != nil ? self.image! : UIImage())
+                    VStack (alignment: .leading) {
+                        Text(self.place.place.name != nil ? self.place.place.name! : "")
+                        Text(self.place.distance != nil ? "\(getDistanceStringToDisplay(self.place.distance!))" : "distance")
+                    }
+                    Spacer()
+                }
+                    .frame(width: metrics.size.width * 0.4)
+                    .onTapGesture {
+                        self.exploreModel.changeCurrentTargetTo(self.place)
+                }
+                HStack {
+                    Button(action: {
+                        print("mark visited")
+                    }) {
+                        HStack {
+                            Image(systemName: "mappin.and.ellipse")
+                            Text("Mark visited")
+                        }
+                    }
+                }.frame(width: metrics.size.width * 0.3)
+                HStack {
+                    Button(action: {
+                        UIApplication.shared.open(getUrlForGoogleMapsNavigation(place: self.place.place))
+                    }) {
+                        HStack {
+                            Image(systemName: "arrow.up.right.diamond")
+                            Text("Navigate")
+                        }
+                    }
+                }.frame(width: metrics.size.width * 0.3)
+            }
+        }
+        .frame(height: 60)
+        .onAppear {
+            if let photos = self.place.place.photos {
+                getPlaceFoto(photoMetadata: photos[0]) { (photo: UIImage?, error: Error?) in
+                    if let error = error {
+                        print("Error loading photo metadata: \(error.localizedDescription)")
+                        return
+                    }
+                    if let photo = photo {
+                        self.image = photo
+                    }
+                }
+            }
+        }
+    }
+}
