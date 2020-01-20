ec27c86ecb112d473b7e35d3fd65f144eeb2e403
hawi <havyha@hotmail.com>
2020-01-20 09:01:14 +0100

    Add photo gallery

--- SpotUp/Views/ItemView/ItemView.swift
+++ SpotUp/Views/ItemView/ItemView.swift
@@ -14,14 +14,14 @@ struct ItemView: View {
     @State var place: GMSPlace? = nil
     @State var isOpen: String? = nil
     @State var priceLevel: Int? = nil
-    @State var types: [String]? = nil
+    @State var type: String? = nil
     
     var body: some View {
         VStack {
             if (place == nil) {
                 Text("")
             } else {
-                InnerItemView(place: place!, isOpen: isOpen!, priceLevel: priceLevel!, types: types!)
+                InnerItemView(place: place!, isOpen: isOpen!, priceLevel: priceLevel!, type: type!)
             }
         }.onAppear {
             getPlace(placeID: self.placeId) { (place: GMSPlace?, error: Error?) in
@@ -33,7 +33,9 @@ struct ItemView: View {
                     self.place = place
                     self.isOpen = getPlaceIsOpenNow(isOpen: place.isOpen(at:NSDate.now))
                     self.priceLevel = getPlacePriceLevel(priceLevel: place.priceLevel)
-                    self.types = place.types
+                    if let types = place.types{
+                    self.type = parseType(types:types)}
+                    dump(place.openingHours)
                     print("The selected place is: \(String(describing: place.name))")
                 }
             }
@@ -45,59 +47,93 @@ struct InnerItemView: View {
     var place: GMSPlace
     var isOpen: String
     var priceLevel:Int
-    var types:[String]
-    
+    var type:String
+    @ObservedObject var photoGallery = Gallery()
     @State var image: UIImage?
+    @State var image2: UIImage?
     @State var showSheet = false
     
     var body: some View {
+        ScrollView{
         VStack {
-            ItemMapView(coordinate: place.coordinate)
-                .edgesIgnoringSafeArea(.top)
-                .frame(height:300)
-            
+            ScrollView{
             HStack {
-                ItemImageView(image: image != nil ? image! : UIImage())
-                    .frame(width: 200,height: 200)
-                    .offset(y: -90)
-                    .padding(.bottom, -90)
+                ForEach(photoGallery.gallery, id:\.self){
+                    photo in
+                    return ItemImageView(image:photo)
+                }
+//                ItemImageView(image: image != nil ? image! : UIImage())
+//
+//                    .offset(y: -90)
+//                    .padding(.bottom, -90)
+                }}
+            
+        //topbar info
+            HStack{
+                //type
+                Text(type)
+                //distance
+                //priceLevel
+                ForEach (drawSigns(signs: getPlacePriceLevel(priceLevel: place.priceLevel), name: "eurosign.circle"), id: \.self) { sign in
+                    Image(systemName:sign)
+                
+                }
+                //addbutton
                 Button(action: {
                     self.showSheet.toggle()
                 }){
-                    Image(systemName: "plus.circle.fill")
-                    .resizable()
-                    .scaledToFit()
-                        .frame(width: 30, height: 30)
+                    HStack {
+                        Image(systemName: "plus")
+                            .font(.body)
+                        Text("add")
+                            .fontWeight(.semibold)
+                            .font(.body)
+                    }
+                    .padding(5)
+                    .foregroundColor(.white)
+                   .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.orange]), startPoint: .leading, endPoint: .trailing))
+                    .cornerRadius(5)
+                    
                 }
             }
-            ScrollView(showsIndicators: false){
-                HStack{
-                    ForEach (drawSigns(signs: getPlacePriceLevel(priceLevel: place.priceLevel), name: "eurosign.circle"), id: \.self) { sign in
-                        Image(systemName:sign)
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 50, maxHeight: 50)
+        .cornerRadius(15)
+            .padding()
+            .offset(y:-30)
+            
+            //main info
+            VStack(alignment: .leading) {
+                Text(place.name != nil ? place.name! : "")
+                    .font(.title)
+                Text(place.formattedAddress != nil ? "\(place.formattedAddress!)" : "")
+                Button(action:{
+                    if let phoneNumber = self.place.phoneNumber{
+                        let prefix = "tel://"
+                        let trimmed = phoneNumber.replacingOccurrences(of: " ", with: "")
+                        let tel = URL(string:(prefix + trimmed))
+                        UIApplication.shared.open(tel!)
                     }
+                }){Text("Call Now").foregroundColor(.red)}
+                Text(place.phoneNumber != nil ? "\(place.phoneNumber!)" : "no phone number")
+                Button(action: {
+                    if let website = self.place.website {
+                        UIApplication.shared.open(website)
+                    }
+                }){
+                    Text("Open Website").foregroundColor(.red)
                 }
-                VStack(alignment: .leading) {
-                    Text(place.name != nil ? place.name! : "")
-                        .font(.title)
-                    Text(place.formattedAddress != nil ? "\(place.formattedAddress!)" : "")
-                    Button(action:{
-                        if let phoneNumber = self.place.phoneNumber{
-                            let prefix = "tel://"
-                            let trimmed = phoneNumber.replacingOccurrences(of: " ", with: "")
-                            let tel = URL(string:(prefix + trimmed))
-                            UIApplication.shared.open(tel!)
-                        }
-                    }){Text("Call Now")}
-                    Text(place.phoneNumber != nil ? "\(place.phoneNumber!)" : "no phone number")
-                    Button(action: {
-                        if let website = self.place.website {
-                            UIApplication.shared.open(website)
-                        }
-                    }){
-                        Text("Open Website")
-                    }
-                Text(isOpen)
-                        }
+            Text(isOpen)
+            }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 300, maxHeight: 300)
+
+            //map
+            VStack{
+            ItemMapView(coordinate: place.coordinate)
+                .edgesIgnoringSafeArea(.top)
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 300, maxHeight: 300)
+
+            }
+            //end
+                
                 Spacer()
             }
         }
@@ -106,6 +142,7 @@ struct InnerItemView: View {
         }
         .navigationBarTitle(Text(place.name != nil ? place.name! : ""), displayMode:.inline)
         .onAppear {
+            self.photoGallery.getGallery(images: self.place.photos)
             if let photos = self.place.photos {
                 getPlaceFoto(photoMetadata: photos[0]) { (photo: UIImage?, error: Error?) in
                     if let error = error {
@@ -115,10 +152,19 @@ struct InnerItemView: View {
                     if let photo = photo {
                         self.image = photo;
                     }
+                    
                 }
+                
             }
         }
     }
+}
+
+
+extension StringProtocol {
+var firstUppercased: String {
+    return prefix(1).uppercased() + dropFirst()
+}
 }
 
 func drawSigns(signs:Int, name:String)->[String]{
@@ -132,3 +178,10 @@ func drawSigns(signs:Int, name:String)->[String]{
     }
     return temp
 }
+
+func parseType(types:[String])->String{
+    let temp = types[0].replacingOccurrences(of: "_", with: " ")
+    return temp.firstUppercased
+    
+}
+
---
+++ SpotUp/Views/ItemView/PhotoGallery.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoGallery.swift
+//  SpotUp
+//
+//  Created by Havy Ha on 18.01.20.
+//
+
+import Foundation
+import GooglePlaces
+import SwiftUI
+
+class Gallery:ObservableObject{
+    @Published var gallery:[UIImage] = []
+    
+    func getGallery (images:[GMSPlacePhotoMetadata]?){
+        if let images = images{
+        var count = 0
+        for image in images{
+            if count < 5 {
+                getPlaceFoto(photoMetadata: image){ (photo: UIImage?, error: Error?) in
+                    if let error = error {
+                        print("Error loading photo metadata: \(error.localizedDescription)")
+                        return
+                    }
+                    if let photo = photo {
+                        self.gallery.append(photo)
+                        count+=1                    }
+                }
+            }
+            else {
+                return
+            }
+        }
+        
+        }}
+}
