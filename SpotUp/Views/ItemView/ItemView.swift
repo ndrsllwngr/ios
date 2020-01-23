0cc8baed76ef5f7fe22ac2f5dd3a4fb4d4f745c3
hawi <havyha@hotmail.com>
2020-01-23 19:37:09 +0100

    Fix loading Scrollview issue

--- SpotUp/Views/ItemView/ItemView.swift
+++ SpotUp/Views/ItemView/ItemView.swift
@@ -17,7 +17,7 @@ struct ItemView: View {
     @State var type: String? = nil
     @State var openingHoursText: [String]? = []
     @State var photos: [GMSPlacePhotoMetadata]? = []
-    @State var gallery: [UIImage] = []
+    @State var gallery: [UIImage] = [UIImage(named: "place_image_placeholder")!]
     
     
     
@@ -28,9 +28,9 @@ struct ItemView: View {
             } else {
                 ScrollView{
                     VStack{
-                    VStack{
-                        GalleryView(gallery: self.$gallery)
-                            }
+                        VStack{
+                            GalleryView(gallery: self.$gallery)
+                        }
                         InnerItemView(place: place!, isOpen: isOpen!, priceLevel: priceLevel!, type: type!, openingHoursText: openingHoursText!, gallery: self.$gallery)
                             .offset(y:-30)
                     }}}
@@ -51,24 +51,27 @@ struct ItemView: View {
                     if let types = place.types{
                         self.type = parseType(types:types)}
                     if let photos = place.photos{
-                        print("photos l.54")
+                        var tmp:[UIImage] = []
                         for (index, image) in photos.enumerated(){
-                        guard index <= 5 else {
-                            print("break")
-                            break}
-                        getPlaceFoto(photoMetadata: image){ (photo: UIImage?, error: Error?) in
-                            if let error = error {
-                                print("Error loading photo metadata: \(error.localizedDescription)")
-                                return
+                            guard index <= 5 else {
+                                print("break")
+                                break}
+                            getPlaceFoto(photoMetadata: image){ (photo: UIImage?, error: Error?) in
+                                if let error = error {
+                                    print("Error loading photo metadata: \(error.localizedDescription)")
+                                    return
+                                }
+                                if let photo = photo {
+                                    tmp.append(photo)
+                                }
+                                if tmp.isEmpty == false{
+                                self.gallery.removeAll()
+                                    self.gallery = tmp}
                             }
-                            if let photo = photo {
-                                self.gallery.append(photo)
-                            }
-                        }
                         }
                         
                     }
-                    
+                    print("Gallery Size: \(String(describing: self.gallery.count))" )
                     print("The selected place is: \(String(describing: place.name))")
                 }
             }
@@ -84,142 +87,116 @@ struct InnerItemView: View {
     var openingHoursText: [String]
     @Binding var gallery: [UIImage]
     @State var showSheet = false
-
+    
     
     
     var body: some View {
-//        ScrollView(showsIndicators: false){
-            VStack{
-                    //---------------------------------------------
-//                    GalleryView(photos: place.photos, gallery: self.$gallery)
-//                        .frame(width:UIScreen.main.bounds.width, height:300)
-                    //---------------------------------------------
-                    //topbar info
-                    HStack(){
-                        //type
-                        Text(type)
-                        //distance
-                        //priceLevel
-                        Spacer()
-                        HStack{
-                            ForEach (drawSigns(signs: getPlacePriceLevel(priceLevel: place.priceLevel), name: "eurosign.circle"), id: \.self) { sign in
-                                Image(systemName:sign)}
-                            
-                        }
-                        Spacer()
-                        //addbutton
-                        Button(action: {
-                            self.showSheet.toggle()
-                        }){
-                            HStack {
-                                Image(systemName: "plus")
-                                    .font(.body)
-                                Text("add")
-                                    .fontWeight(.semibold)
-                                    .font(.body)
-                            }
-                            .padding(5)
-                            .foregroundColor(.white)
-                            .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.orange]), startPoint: .leading, endPoint: .trailing))
-                            .cornerRadius(5)
-                            
-                        }
-                        
-                    } // end stack top bar
-                        .padding(.horizontal,5)
-                        .frame(width:UIScreen.main.bounds.width, height:50)
-                        .background(Color.white)
-                        .cornerRadius(15)
+        //        ScrollView(showsIndicators: false){
+        VStack{
+            //---------------------------------------------
+            //                    GalleryView(photos: place.photos, gallery: self.$gallery)
+            //                        .frame(width:UIScreen.main.bounds.width, height:300)
+            //---------------------------------------------
+            //topbar info
+            HStack(){
+                //type
+                Text(type)
+                //distance
+                //priceLevel
+                Spacer()
+                HStack{
+                    ForEach (drawSigns(signs: getPlacePriceLevel(priceLevel: place.priceLevel), name: "eurosign.circle"), id: \.self) { sign in
+                        Image(systemName:sign)}
                     
-                    
-                //end Zstack
-                
-                //---------------------------------------------
-                //main info
-                VStack(alignment:.leading, spacing: 10){
-                    Text(place.name != nil ? place.name! : "")
-                        .font(.title)
-                    Text(place.formattedAddress != nil ? "\(place.formattedAddress!)" : "")
-                    Button(action:{
-                        if let phoneNumber = self.place.phoneNumber{
-                            let prefix = "tel://"
-                            let trimmed = phoneNumber.replacingOccurrences(of: " ", with: "")
-                            let tel = URL(string:(prefix + trimmed))
-                            UIApplication.shared.open(tel!)
-                        }
-                    }){Text("Call Now").foregroundColor(.red)}
-                    Text(place.phoneNumber != nil ? "\(place.phoneNumber!)" : "no phone number")
-                    Button(action: {
-                        if let website = self.place.website {
-                            UIApplication.shared.open(website)
-                        }
-                    }){
-                        Text("Open Website").foregroundColor(.red)
+                }
+                Spacer()
+                //addbutton
+                Button(action: {
+                    self.showSheet.toggle()
+                }){
+                    HStack {
+                        Image(systemName: "plus")
+                            .font(.body)
+                        Text("add")
+                            .fontWeight(.semibold)
+                            .font(.body)
                     }
-                    Text(isOpen)}
-                    .frame(width:UIScreen.main.bounds.width-10, height:300)
-                //---------------------------------------------
-                //map
-                VStack(alignment: .center){
-                    ItemMapView(coordinate: place.coordinate)}.cornerRadius(15)
-                    .frame(width:UIScreen.main.bounds.width-10, height:220)
-                //end
-                //---------------------------------------------
-                VStack{
-                    ScrollWeekView(data: createDateCardData(openingHoursText: openingHoursText))
-                }.frame(width:UIScreen.main.bounds.width-10, height:250)
+                    .padding(5)
+                    .foregroundColor(.white)
+                    .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.orange]), startPoint: .leading, endPoint: .trailing))
+                    .cornerRadius(5)
+                    
+                }
                 
-                
-            }// end Vstack
-//        }// end ScrollView
+            } // end stack top bar
+                .padding(.horizontal,5)
+                .frame(width:UIScreen.main.bounds.width, height:50)
+                .background(Color.white)
+                .cornerRadius(15)
+            
+            
+            //end Zstack
+            
+            //---------------------------------------------
+            //main info
+            VStack(alignment:.leading, spacing: 10){
+                Text(place.name != nil ? place.name! : "")
+                    .font(.title)
+                Text(place.formattedAddress != nil ? "\(place.formattedAddress!)" : "")
+                Button(action:{
+                    if let phoneNumber = self.place.phoneNumber{
+                        let prefix = "tel://"
+                        let trimmed = phoneNumber.replacingOccurrences(of: " ", with: "")
+                        let tel = URL(string:(prefix + trimmed))
+                        UIApplication.shared.open(tel!)
+                    }
+                }){Text("Call Now").foregroundColor(.red)}
+                Text(place.phoneNumber != nil ? "\(place.phoneNumber!)" : "no phone number")
+                Button(action: {
+                    if let website = self.place.website {
+                        UIApplication.shared.open(website)
+                    }
+                }){
+                    Text("Open Website").foregroundColor(.red)
+                }
+                Text(isOpen)}
+                .frame(width:UIScreen.main.bounds.width-10, height:300)
+            //---------------------------------------------
+            //map
+            VStack(alignment: .center){
+                ItemMapView(coordinate: place.coordinate)}.cornerRadius(15)
+                .frame(width:UIScreen.main.bounds.width-10, height:220)
+            //end
+            //---------------------------------------------
+            VStack{
+                ScrollWeekView(data: createDateCardData(openingHoursText: openingHoursText))
+            }.frame(width:UIScreen.main.bounds.width-10, height:250)
+            
+            
+        }// end Vstack
+            //        }// end ScrollView
             .sheet(isPresented: $showSheet) {
                 ItemAddSheet(place: self.place, placeImage: self.gallery.indices.contains(0) ? self.gallery[0] : nil, showSheet: self.$showSheet)
                 
         }
         .navigationBarTitle(Text(place.name != nil ? place.name! : ""), displayMode:.inline)
-//        .onAppear {
-//            self.photoGallery.getGallery(images: self.place.photos)
-//        }
+        
+        //        }
     }//end body
 }//end wholew View
 
 
 struct GalleryView:View{
-//    var photos: [GMSPlacePhotoMetadata]?
     @Binding var gallery: [UIImage]
-//    @ObservedObject var photoGallery : Gallery = Gallery()
-    
     var body : some View {
-            ScrollView(.horizontal,showsIndicators: false){
-                HStack (spacing:5) {
-                    ForEach(self.gallery, id:\.self){
-                        photo in
-                        return HStack{ ItemImageView(image:photo)} .frame(width:UIScreen.main.bounds.width, height:300)
-                    }
+        ScrollView(.horizontal,showsIndicators: false){
+            HStack (spacing:5) {
+                ForEach(self.gallery, id:\.self){
+                    photo in
+                    return HStack{ ItemImageView(image:photo)} .frame(width:UIScreen.main.bounds.width, height:300)
                 }
             }
-//        .onAppear {
-//            if let photos = self.photos {
-//                    for (index, image) in photos.enumerated(){
-//                        guard index <= 5 else {
-//                            break}
-//                        getPlaceFoto(photoMetadata: image){ (photo: UIImage?, error: Error?) in
-//                            if let error = error {
-//                                print("Error loading photo metadata: \(error.localizedDescription)")
-//                                return
-//                            }
-//                            if let photo = photo {
-//                                self.gallery.append(photo)
-//                            }
-//                        }
-//                        //            else {
-//                        //                return
-//                        //            }
-//                    }
-//                self.photoGallery.getGallery(images: photos)} else {
-//                self.gallery.append(UIImage(named: "place_image_placeholder")!)
-//            }
-//        }
+        }
     }
 }
 
@@ -227,6 +204,7 @@ struct GalleryView:View{
 struct DateCardView: View {
     var day: String
     var hours: String
+
     
     var body: some View {
         VStack {
