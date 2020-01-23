a8c21ce04ac242309a0b630d44d6e2c8e157344d
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2020-01-23 17:08:07 +0100

    Add empty states to search
    .

--- SpotUp/Views/HomeView/SearchResultsEmptyStateView.swift
+++ SpotUp/Views/HomeView/SearchResultsEmptyStateView.swift
@@ -4,10 +4,18 @@ struct SearchResultsProfilesEmptyStateView: View {
     var body: some View {
         VStack() {
             Spacer()
+            VStack() {
+                Spacer()
             Image(uiImage: UIImage(named: "placeholder-search-1")!)
             .renderingMode(.original)
             .resizable()
-            Text("Empty state")
+            .scaledToFit()
+                .frame(width: 250.0, height: 250.0, alignment: .center)
+                Text("Add friends").font(.title).multilineTextAlignment(.center)
+                Text("Find your friends or locals to see their\n hot spots collections and\n recommendations.").multilineTextAlignment(.center).padding(.horizontal)
+                
+            Spacer()
+            }.padding(.horizontal)
             Spacer()
         }
     }
@@ -23,10 +31,18 @@ struct SearchResultsListsEmptyStateView: View {
     var body: some View {
         VStack() {
             Spacer()
+            VStack() {
+                Spacer()
             Image(uiImage: UIImage(named: "placeholder-search-3")!)
             .renderingMode(.original)
             .resizable()
-            Text("Empty state")
+            .scaledToFit()
+                .frame(width: 250.0, height: 250.0, alignment: .center)
+                Text("Discover new spots").font(.title).multilineTextAlignment(.center)
+                Text("Search collection lists of places to\n explore recommended spots\n by other users.").font(.body).multilineTextAlignment(.center).padding(.horizontal)
+                
+            Spacer()
+            }.padding(.horizontal)
             Spacer()
         }
     }
@@ -42,10 +58,18 @@ struct SearchResultsPlacesEmptyStateView: View {
     var body: some View {
         VStack() {
             Spacer()
+            VStack() {
+                Spacer()
             Image(uiImage: UIImage(named: "placeholder-search-2")!)
             .renderingMode(.original)
             .resizable()
-            Text("Empty state")
+            .scaledToFit()
+                .frame(width: 250.0, height: 250.0, alignment: .center)
+                Text("Find places").font(.title).multilineTextAlignment(.center)
+                Text("Look up places to get more\n information and options to save it\n in a collection.").multilineTextAlignment(.center).padding(.horizontal)
+                
+            Spacer()
+            }
             Spacer()
         }
     }
