b5b881e66f5fddabafb388eb7ffc7d59e1d7bc2d
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2020-01-25 17:58:35 +0100

    Init redesign of login/signup view
    .

--- SpotUp/Views/TabBarView.swift
+++ SpotUp/Views/TabBarView.swift
@@ -16,55 +16,67 @@ struct TabBarView: View {
     
     var body: some View {
         GeometryReader { metrics in
-            VStack(spacing: 0) {
-                VStack {
-                    if (self.selection == 0) {
-                        NavigationView {
-                            ExploreView(tabSelection: self.$selection)
-                        }
-                        
-                    } else if (self.selection == 1) {
-                        NavigationView {
-                            SearchView(tabSelection: self.$selection)
-                        }
-                    } else if (self.selection == 2) {
-                        NavigationView {
-                            ProfileView(profileUserId: self.firebaseAuthentication.currentUser!.uid, tabSelection: self.$selection)
+            ZStack{
+                VStack(spacing: 0) {
+                    HStack(spacing: 0){
+                        Spacer()
+                    }
+                    Spacer()
+                }
+                .frame(minHeight: 0, maxHeight: .infinity)
+                    //                .background(Color.red)
+                    .background(Color("bg-tab-bar"))
+                    .edgesIgnoringSafeArea(.bottom)
+                VStack(spacing: 0) {
+                    VStack(spacing: 0) {
+                        if (self.selection == 0) {
+                            NavigationView {
+                                ExploreView(tabSelection: self.$selection)
+                            }
                             
+                        } else if (self.selection == 1) {
+                            NavigationView {
+                                SearchView(tabSelection: self.$selection)
+                            }
+                        } else if (self.selection == 2) {
+                            NavigationView {
+                                ProfileView(profileUserId: self.firebaseAuthentication.currentUser!.uid, tabSelection: self.$selection)
+                                
+                            }
                         }
                     }
-                }
-                Spacer()
-                if (self.exploreModel.exploreList != nil && self.selection != 0) {
-                    ExploreIsActiveBar(tabSelection: self.$selection)
-                }
-                HStack {
                     Spacer()
-                    VStack {
-                        Image(systemName: self.selection == 0 ? "map.fill" : "map")
-                        Text("Explore")
-                        
-                    }.onTapGesture {
-                        self.selection = 0
+                    if (self.exploreModel.exploreList != nil && self.selection != 0) {
+                        ExploreIsActiveBar(tabSelection: self.$selection)
                     }
-                    Spacer()
-                    VStack {
-                        Image(systemName: self.selection == 1 ? "magnifyingglass.circle.fill" : "magnifyingglass.circle")
-                        Text("Search")
-                    }.onTapGesture {
-                        self.selection = 1
+                    HStack(spacing: 0) {
+                        Spacer()
+                        VStack {
+                            Image(systemName: self.selection == 0 ? "map.fill" : "map")
+                            Text("Explore")
+                            
+                        }.onTapGesture {
+                            self.selection = 0
+                        }
+                        Spacer()
+                        VStack {
+                            Image(systemName: self.selection == 1 ? "magnifyingglass.circle.fill" : "magnifyingglass.circle")
+                            Text("Search")
+                        }.onTapGesture {
+                            self.selection = 1
+                        }
+                        Spacer()
+                        VStack {
+                            Image(systemName: self.selection == 2 ? "person.fill" : "person")
+                            Text("Profile")
+                        }.onTapGesture {
+                            self.selection = 2
+                        }
+                        Spacer()
                     }
-                    Spacer()
-                    VStack {
-                        Image(systemName: self.selection == 2 ? "person.fill" : "person")
-                        Text("Profile")
-                    }.onTapGesture {
-                        self.selection = 2
-                    }
-                    Spacer()
+                    .frame(width: metrics.size.width, height: 50)
+                    .background(Color("bg-tab-bar"))
                 }
-                .frame(width: metrics.size.width, height: 50)
-                .background(Color("bg-tab-bar"))
             }
             // TODO colore on the back
             //.edgesIgnoringSafeArea(.bottom)
